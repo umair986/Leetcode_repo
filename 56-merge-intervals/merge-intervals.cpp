1f721d126a525803a14952661e7d166da76ac084
Mohammed Umair Shaikh <69787249+umair986@users.noreply.github.com>
2024-06-05 23:58:10 +0530

    Time: 24 ms (50.80%) | Memory: 22.7 MB (68.45%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end());
+         vector<vector<int>> ans;
+        for (int i = 0; i<n; i++){
+            int startposi = intervals[i][0];
+            int endposi = intervals[i][1];
+            if(!ans.empty() && endposi <=ans.back()[1]){
+            continue;
+            }
+
+            for(int j = i+1; j<n; j++){
+                if(intervals[j][0] <= endposi){
+                endposi = max(endposi, intervals[j][1]);
+                }
+                else{
+                 break;
+                }
+            }
+            ans.push_back({startposi , endposi});
+        }
+        return ans;
+    }  
+};
