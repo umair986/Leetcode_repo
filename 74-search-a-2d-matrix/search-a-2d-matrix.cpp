01f72e589c6ddaca27460ea498232a468a84c4db
Mohammed Umair Shaikh <69787249+umair986@users.noreply.github.com>
2024-06-10 23:19:46 +0530

    Time: 7 ms (21.71%) | Memory: 11.9 MB (48.44%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+      if (matrix.empty() || matrix[0].empty()) {
+        return false;
+    }
 
     int rows = matrix.size();
     int cols = matrix[0].size();
